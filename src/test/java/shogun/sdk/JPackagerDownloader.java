5491886a00e352807fc76880347705e1128439eb
Yusuke Yamamoto <yusuke@mac.com>
2019-07-07 16:42:17 +0900

    shogun-2 Package as a macOS application Automatic jdk.packager downloader

---
+++ src/test/java/shogun/sdk/JPackagerDownloader.java
@@ -0,0 +1,88 @@
+package shogun.sdk;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+class JPackagerDownloader {
+    @Test
+    void ensureJpackagerExists() throws IOException {
+        // https://mail.openjdk.java.net/pipermail/openjfx-dev/2018-September/022500.html
+        Path packagerRoot = Paths.get("jdk.packager");
+        if (!Files.exists(packagerRoot)) {
+            String fileName = "jdk.packager-";
+            String os = System.getProperty("os.name").toLowerCase();
+            boolean isWindows = false;
+            if (os.contains("nux")) {
+                fileName += "linux";
+            } else if (os.startsWith("windows")) {
+                fileName += "windows";
+                isWindows = true;
+            } else if (os.contains("mac") || os.contains("darwin")) {
+                fileName += "osx";
+            } else {
+                throw new IllegalStateException("Unsupported os:" + os);
+            }
+            fileName += ".zip";
+            Files.createDirectory(packagerRoot);
+            String downloadURL = "http://download2.gluonhq.com/jpackager/11/" + fileName;
+            URL url = new URL(downloadURL);
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+                int code = conn.getResponseCode();
+                if (code == 200) {
+                    Files.copy(conn.getInputStream(), packagerRoot.resolve(fileName));
+                } else {
+                    throw new IOException("URL[" + url + "] returns code [" + code + "].");
+                }
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+            unZip(packagerRoot, Paths.get("jdk.packager" + File.separator + fileName));
+            Path bin = packagerRoot.resolve("jpackager" + (isWindows ? ".exe" : ""));
+            //noinspection ResultOfMethodCallIgnored
+            bin.toFile().setExecutable(true);
+            if (isWindows) {
+                // jpackager.exe needs to be located in %JAVA_HOME%\bin
+                //noinspection Since15
+                Files.move(bin, Path.of(System.getProperty("java.home") + File.separator + "bin" + File.separator + "jpackager.exe"));
+                // jdk.packager.jar needs to be located in %JAVA_HOME%\jmods
+                //noinspection Since15
+                Files.move(packagerRoot.resolve("jdk.packager.jar"), Path.of(System.getProperty("java.home") + File.separator + "jmods" + File.separator + "jdk.packager.jar"));
+            }
+        }
+    }
+
+
+    private static void unZip(Path root, Path archiveFile) throws IOException {
+        ZipFile zip = new ZipFile(archiveFile.toFile());
+        Enumeration<? extends ZipEntry> entries = zip.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            if (entry.isDirectory()) {
+                Files.createDirectories(root.resolve(entry.getName()));
+            } else {
+                try (InputStream is = new BufferedInputStream(zip.getInputStream(entry))) {
+                    Files.copy(is, root.resolve(entry.getName()));
+                }
+            }
+
+        }
+    }
+
+}
+
