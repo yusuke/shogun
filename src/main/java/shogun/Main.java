ed804e29704a1a1f377c2ca0ef0a5e0666a99286
Yusuke Yamamoto <yusuke@mac.com>
2019-07-10 01:20:37 +0900

    fixes #15 Migrate to Java 11. Update maven plugins

--- src/main/java/shogun/Main.java
+++ src/main/java/shogun/Main.java
@@ -1,7 +1,9 @@
+package shogun;
+
 import shogun.task.TaskTray;
 
 
-public class Shogun {
+public class Main {
     public static void main(String[] args) {
         System.setProperty("apple.awt.UIElement", "true");
         new TaskTray().show();
