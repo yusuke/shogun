17792344a80858dc4863a3b7dbf0e3d806a22a32
Yusuke Yamamoto <yusuke@mac.com>
2019-07-21 20:53:32 +0900

    store log files in USER_HOME/Shogun/Log on Windows|Linux platform

--- src/main/java/shogun/Shogun.java
+++ src/main/java/shogun/Shogun.java
@@ -1,11 +1,23 @@
 package shogun;
 
+import shogun.sdk.Platform;
 import shogun.task.TaskTray;
 
+import java.io.File;
 
 public class Shogun {
     public static void main(String[] args) {
-        System.setProperty("apple.awt.UIElement", "true");
+        // Set ${log.path} in logback.xml
+        // Use of Platform.isMac should be avoided here because log.path need to be set before logback gets initialized
+        if (System.getProperty("os.name").toLowerCase().matches("^.*(mac|darwin).*$")) {
+            // macOS
+            System.setProperty("log.path", System.getProperty("user.home") + "/Library/Logs/Shogun".replaceAll("/", File.separator));
+        } else {
+            // Windows or  Linux
+            System.setProperty("log.path", System.getProperty("user.home") + "/Shogun/Logs".replaceAll("/", File.separator));
+        }
+
+        Platform.isMac(() -> System.setProperty("apple.awt.UIElement", "true"));
         new TaskTray().show();
     }
 }
